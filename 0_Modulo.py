2baf6b6b393f61aa304737915c5572028c7dbbf3
Jeff Miranda <jefferson_miranda@hotmail.es>
2021-04-02 18:57:34 -0500

    modificacion ficheros

---
+++ 0_Modulo.py
@@ -0,0 +1,8 @@
+#averiguar modulos
+#flask - crea aplicaciones web
+#django - framework (modelo / vista / controlador)
+#tkinter - interfaces graficas de usuario
+
+esto es python
+git commit -m "first commit"
+xxxx
