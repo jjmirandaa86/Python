c2671dd99beedb8991e1b7385009d92a09b769e1
Jeff Miranda <jefferson_miranda@hotmail.es>
2021-04-02 20:20:06 -0500

    liso2

--- 0_Modulo.py
+++
@@ -1,8 +0,0 @@
-#averiguar modulos
-#flask - crea aplicaciones web
-#django - framework (modelo / vista / controlador)
-#tkinter - interfaces graficas de usuario
-
-esto es python
-git commit -m "first commit"
-xxxx
